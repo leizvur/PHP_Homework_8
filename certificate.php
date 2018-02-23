6e95fe944a9ad99bad6463e25d0e988035a2a63f
leizvur <35609834+leizvur@users.noreply.github.com>
2018-02-23 23:36:59 +0300

    правки по замечаниям преподавателя

--- certificate.php
+++ certificate.php
@@ -1,35 +1,35 @@
 <?php
 
 //Текст сертификата
-$cert_text="Этот сертификат подтверждает, что ты на 33% рожден в СССР!";
-session_start();
-$_SESSION["code"]=$cert_text;
+$certText="Этот сертификат подтверждает, что ты на 33,3% рожден в СССР!";
+//session_start();
+//$_SESSION["code"]=$certText;
 
 //Картинка сертификата
 $image=imagecreatetruecolor(640, 320);
-$background_color=imagecolorallocate($image, 255, 0, 0);
-$text_color=imagecolorallocate($image, 0, 0, 255);
+$backgroundColor=imagecolorallocate($image, 255, 0, 0);
+$textColor=imagecolorallocate($image, 0, 255, 0);
 
-$cert_png=__DIR__ . "/certificate.png";
-if (!file_exists($cert_png)) 
+$certPng=__DIR__ . "/certificate.png";
+if (!file_exists($certPng)) 
 {
 	echo "Файл с картинкой для сертификата не найден!";
 	exit;
 }
 
 
-$font_ttf=__DIR__ . '/impact.ttf';
-if (!file_exists($font_ttf)) 
+$fontTtf=__DIR__ . '/impact.ttf';
+if (!file_exists($fontTtf)) 
 {
 	echo "Файл шрифта не найден!";
 	exit;
 }
 
 
-$cert_file=imagecreatefrompng($cert_png);
-imagefill($image, 20, 20, $background_color);
-imagecopy($image, $cert_png, 0, 0, 630, 320, 630, 320);
-imagettftext($image, 15, 0, 50, 100, $text_color, $font_ttf, $cert_text);
+$certFile=imagecreatefrompng($certPng);
+imagefill($image, 20, 20, $backgroundColor);
+imagecopy($image, $certFile, 0, 0, 0, 0, 640, 320);
+imagettftext($image, 15, 0, 50, 200, $textColor, $fontTtf, $certText);
 header('Content-Type: image/png');
 imagepng($image);
 
